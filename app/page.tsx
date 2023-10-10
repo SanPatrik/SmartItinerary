ac93756a3ac7522ee3f56f03dfabc3c9fd466488
PatrikAntal <patrik.antal3@gmail.com>
2023-10-10 11:53:55 +0200

    created develop branch

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { ChatWindow } from "@/components/ChatWindow";
 
+
 export default function Home() {
     const InfoCard = (
         <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden">
