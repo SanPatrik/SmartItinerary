738f8f91e3dd4be2ec78ca4032047ca3d374023f
Oskar M <mihalikoskar@gmail.com>
2024-02-24 19:41:15 +0100

    chore: comment cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,6 @@ export default function Home() {
     return (
         <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden">
             <h1 className="text-3xl md:text-4xl mb-4">▲ Smart Itinerary 🔗</h1>
-            {/* <form action={async (formData: FormData)=> {
-                redirect('itenirary/?prompt='+formData.get('prompt'))
-            }}>
-                <input type="text" name="prompt" placeholder="Where do you want to go?" />
-            </form>             */}
             <Suspense fallback={<div>Loading...</div>}>
                 <Form/>
             </Suspense>
