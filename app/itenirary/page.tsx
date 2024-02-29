59ef737f6a7a62f4aafbe42aa60acde7f5ee01e7
Oskar M <mihalikoskar@gmail.com>
2024-02-29 21:51:00 +0100

    feat: places

--- app/itenirary/page.tsx
+++ app/itenirary/page.tsx
@@ -2,15 +2,11 @@ import { Itinerary } from "@/components/Itenirary";
 import { redirect } from "next/navigation";
 import React, { Suspense } from "react";
 
-export default function IteniraryPage({
-    searchParams,
-  }: {
-    searchParams: { [key: string]: string }
-  }) {
+export default function IteniraryPage({ searchParams }: { searchParams: { [key: string]: string } }) {
     return (
-        <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden">
+        <div className="p-4 md:p-8 rounded bg-[#25252d] w-full">
             <Suspense fallback={<div>Loading...</div>}>
-                <Itinerary prompt={searchParams?.['prompt'] ?? ""} />
+                <Itinerary prompt={searchParams?.["prompt"] ?? ""} />
             </Suspense>
         </div>
     );
--- components/Places.tsx
+++ components/Places.tsx
@@ -1,23 +1,36 @@
-import { ItineraryLocationSchema } from '@/ServerActions/getItenirary';
-import React from 'react';
+import { ItineraryLocationSchema } from "@/ServerActions/getItenirary";
+import React from "react";
 
 type PlacesProps = {
-    locations: ItineraryLocationSchema
-}
+    locations: ItineraryLocationSchema;
+};
 
 export const Places = (props: PlacesProps) => {
-    
     return (
-        <div className="flex flex-row">
-            {/* Your component content goes here */}
+        <div className="flex flex-row gap-10">
+            {props.locations.hotels.map((name, index) => {
+                return <Place key={name} placeName={name} />;
+            })}
+            {props.locations.activities.map((name, index) => {
+                return <Place key={name} placeName={name} />;
+            })}
+            {props.locations.shops.map((name, index) => {
+                return <Place key={name} placeName={name} />;
+            })}
+            {props.locations.restaurants.map((name, index) => {
+                return <Place key={name} placeName={name} />;
+            })}
+            {props.locations.cafes.map((name, index) => {
+                return <Place key={name} placeName={name} />;
+            })}
         </div>
     );
 };
 
-// type PlaceProps = {
-//     location: ItineraryLocationSchema
-// }
+type PlaceProps = {
+    placeName: string;
+};
 
-// const Place = (props: PlaceProps) => {
-
-// }
+const Place = (props: PlaceProps) => {
+    return <div className="w-40 h-40 bg-green-50 text-black">{props.placeName}</div>;
+};
