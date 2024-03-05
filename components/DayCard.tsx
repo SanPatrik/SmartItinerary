4289f33bf81a15ff7fee8d318af12dae7a2330b9
vrabelmarian <mrn.vrabel@gmail.com>
2024-03-05 12:24:33 +0100

    feat: DayCard css

--- components/DayCard.tsx
+++ components/DayCard.tsx
@@ -1,23 +1,19 @@
-import {
-    GetIteniraryMock,
-    ItineraryDayInfoSchema,
-    ItineraryDaySchema,
-    ItinerarySchema
-} from "@/ServerActions/getItenirary";
+import { ItineraryDaySchema } from "@/ServerActions/getItenirary";
 
 const DayCard = async ({ data }: { data: ItineraryDaySchema }) => {
     return (
-        <div>
+        <div className="max-w-md mx-auto bg-white shadow-lg rounded-md overflow-hidden">
             {data.timeOfDay.map((timeOfDay, timeIndex) => (
-                <div>
-                    <h3>{timeOfDay.time}</h3>
-                    <p>{timeOfDay.description}</p>
-                    <p>{timeOfDay.activities}</p>
+                <div key={timeIndex} className="p-2 text-gray-800">
+                    <div>
+                        <span className="font-bold">{timeOfDay.time}. </span>
+                        {timeOfDay.description}
+                    </div>
+                    <div className="mt-2 text-blue-500">{timeOfDay.activities}</div>
                 </div>
             ))}
         </div>
     )
 }
-
 
 export default DayCard;
