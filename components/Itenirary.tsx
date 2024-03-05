f872b31a577038ea21f8cf1da8d822213a36bc13
Oskar M <mihalikoskar@gmail.com>
2024-03-05 16:39:02 +0100

    feat: combined day and locations

--- components/Itenirary.tsx
+++ components/Itenirary.tsx
@@ -4,23 +4,25 @@ import DayCard from "@/components/DayCard";
 
 type Props = {
     prompt: string;
-}
+};
 
 export async function Itinerary(props: Props) {
     const itenirary = await GetIteniraryMock(props.prompt);
     if (!itenirary) {
-        return <div>Failed to load itinerary</div>
+        return <div>Failed to load itinerary</div>;
     }
 
-    const locations = itenirary?.days[0]?.timeOfDay[0]?.locations
-    const dayData = itenirary?.days[0]
+    const locations = itenirary?.days[0]?.timeOfDay[0]?.locations;
+    const dayData = itenirary?.days[0];
 
     return (
-        <div className="p-4 md:p-8 rounded ">
-            <h1 className="text-3xl md:text-4xl mb-4">▲ Smart Itinerary 🔗</h1>
-            {JSON.stringify(itenirary)}
-            <Places locations={locations} />
-            <DayCard data={dayData} />
+        <div className=" md:p-4 rounded gap-10 flex w-full">
+            <div className="flex flex-col gap-10">
+                {itenirary.days.map((day, index) => (
+                    <DayCard data={day} key={index} />
+                ))}
+            </div>
+            <div className="w-6/12">map</div>
         </div>
     );
 }
